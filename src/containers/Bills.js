ad0525eb28acfef66c6fa5df793d30bb3f904997
Dequé <carolinedeque@gmail.com>
2022-08-27 17:51:20 +0200

    Sorting bills on bills.js

--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -41,24 +41,31 @@ export default class {
         .bills()
         .list()
         .then((snapshot) => {
-          const bills = snapshot.map((doc) => {
-            try {
-              return {
-                ...doc,
-                date: formatDate(doc.date),
-                status: formatStatus(doc.status),
-              };
-            } catch (e) {
-              // if for some reason, corrupted data was introduced, we manage here failing formatDate function
-              // log the error and return unformatted date in that case
-              console.log(e, "for", doc);
-              return {
-                ...doc,
-                date: doc.date,
-                status: formatStatus(doc.status),
-              };
-            }
-          });
+          // to sort bills from earliest to latest on bill page
+          const bills = snapshot
+            .sort((a, b) => {
+              if (a.date > b.date) {
+                return -1;
+              }
+            })
+            .map((doc) => {
+              try {
+                return {
+                  ...doc,
+                  date: formatDate(doc.date),
+                  status: formatStatus(doc.status),
+                };
+              } catch (e) {
+                // if for some reason, corrupted data was introduced, we manage here failing formatDate function
+                // log the error and return unformatted date in that case
+                console.log(e, "for", doc);
+                return {
+                  ...doc,
+                  date: doc.date,
+                  status: formatStatus(doc.status),
+                };
+              }
+            });
           console.log("length", bills.length);
           return bills;
         });
